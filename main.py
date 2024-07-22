872e3c8e0fdc038cdb2c14b568e5335e41e5df34
Bogdan111022 <addresing88@gmail.com>
2024-07-22 16:31:06 +0300

    lessoo=n2

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import *
+import random 
+import database
 
 app = QApplication([])
 window = QWidget()
@@ -13,8 +15,11 @@ var3_btn = QRadioButton("3")
 var4_btn = QRadioButton("4")
 answer_btn = QPushButton("Відповісти")
 group = QGroupBox("Варіанти відповідей")
-
+res_lbl = Qlable("Результат")
 main_line = QVBoxLayout()
+group_main_line.addWidget(res_lbl)
+group_main_line
+
 
 horisontal_line1 = QHBoxLayout()
 horisontal_line1.addWidget(menu_btn)
@@ -33,6 +38,19 @@ group_main_line.addWidget(var4_btn)
 group.setLayout(group_main_line)
 main_line.addWidget(group)
 main_line.addWidget(answer_btn)
+
+answers = [var1_btn, var2_btn, var3_btn, var4_btn]
+
+def set_quest():
+    random.shuffle(answers)
+    current_question = database.question[database.nomer]
+    quest_lbl.setText(current_question["запитання"])
+    answers[0].setText(current_question["Правельна відповідь"])
+    answers[1].setText(current_question["Не правельна відповідь1"])
+    answers[2].setText(current_question["Не правельна відповідь2"])
+    answers[3].setText(current_question["Не правельна відповідь3"])
+set_quest()
+
 
 
 window.setLayout(main_line)
